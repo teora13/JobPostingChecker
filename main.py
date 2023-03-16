5bd7fe01e14ce9bfd8bfba622d322605abf3d121
Ana Mentus <anmentus@gmail.com>
2023-03-16 10:26:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,9 @@ from bs4 import BeautifulSoup
 link = ('https://www.linkedin.com')
     html_text = requests.get(link).text
     soup = BeautifulSoup(html_text, 'html.parser')
+    
+    job_list = []
+    result = soup.find_all("a", {"class": "hidden-nested-link"})
+    for i in result:
+        job_list.append(i.text.strip())
+    print(job_list)
