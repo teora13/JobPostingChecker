b289b395f73ca74089eb8938be46c444c13713bc
Ana Mentus <anmentus@gmail.com>
2023-04-07 11:01:56 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,20 @@ eel.init('web')
 
 def JPC():
     link = ('https://www.linkedin.com')
-        html_text = requests.get(link).text
-        soup = BeautifulSoup(html_text, 'html.parser')
+    html_text = requests.get(link).text
+    soup = BeautifulSoup(html_text, 'html.parser')
 
-        job_list = []
-        result = soup.find_all("a", {"class": "hidden-nested-link"})
-        for i in result:
-            job_list.append(i.text.strip())
+    job_list = []
+    result = soup.find_all("a", {"class": "hidden-nested-link"})
+    for i in result:
+        job_list.append(i.text.strip())
 
-        df = pandas.read_csv('job_CS.csv', usecols=[3], header=None).dropna()
-        csv_list = df.values.tolist()
+    df = pandas.read_csv('job_CS.csv', usecols=[3], header=None).dropna()
+    csv_list = df.values.tolist()
 
-        company_list = []
-        for company_name in job_list:
-            if any(company_name in el for el in csv_list):
-                company_list.append(' ' + company_name)
+    company_list = []
+    for company_name in job_list:
+       if any(company_name in el for el in csv_list):
+           company_list.append(' ' + company_name)
+    return company_list
+JPC()             
