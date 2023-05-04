3afc38f42ceb9c8dfbae8e5fe04b6225d9253c72
Ana Mentus <anmentus@gmail.com>
2023-05-04 09:53:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -16,10 +16,11 @@ def JPC():
     result = soup.find_all("a", {"class": "hidden-nested-link"})
     for i in result:
         job_list.append(i.text.strip())
-# 
+
     df = pandas.read_csv('job_CS.csv', usecols=[3], header=None).dropna()
     csv_list = df.values.tolist()
 
+# finds the same companies on the page and in the csv file
     company_list = []
     for company_name in job_list:
        if any(company_name in el for el in csv_list):
